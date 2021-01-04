b1ad50be383ce9b08261464c98032b62b476e51f
Max R <rgmasim@gmail.com>
2021-01-04 23:15:03 +0300

    Add TestTestInterfaceWithRateLimit_F_HugeRPS test.

--- templates_tests/interface_with_ratelimit_test.go
+++ templates_tests/interface_with_ratelimit_test.go
@@ -33,3 +33,39 @@ func TestTestInterfaceWithRateLimit_F(t *testing.T) {
 	counter = atomic.LoadUint64(&impl.callCounter)
 	assert.EqualValues(t, 6, counter) //after bust we should receiv 1 request per 100 milliseconds
 }
+
+func TestTestInterfaceWithRateLimit_F_HugeRPS(t *testing.T) {
+	impl := &testImpl{r1: "1", r2: "2", delay: 100 * time.Millisecond}
+
+	wrapped := NewTestInterfaceWithRateLimit(impl, 3, 1000)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			r1, r2, err := wrapped.F(context.Background(), "a1")
+			assert.NoError(t, err)
+			assert.Equal(t, "1", r1)
+			assert.Equal(t, "2", r2)
+
+		}()
+	}
+
+	<-time.After(10 * time.Millisecond)
+
+	counter := atomic.LoadUint64(&impl.callCounter)
+	assert.EqualValues(t, 3, counter) // the first burst
+
+	<-time.After(100 * time.Millisecond)
+
+	counter = atomic.LoadUint64(&impl.callCounter)
+	assert.EqualValues(t, 6, counter) // the second burst
+
+	<-time.After(100 * time.Millisecond)
+
+	counter = atomic.LoadUint64(&impl.callCounter)
+	assert.EqualValues(t, 9, counter) // the third burst
+
+	<-time.After(100 * time.Millisecond)
+
+	counter = atomic.LoadUint64(&impl.callCounter)
+	assert.EqualValues(t, 10, counter) // the 10th call
+}
